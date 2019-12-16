9f189a01de2f4d3cbdbf9e5fcaf62281eb17a28d
Robert Dennison <rdennison11@gmail.com>
2019-12-16 20:50:40 +0000

    Added files via upload

---
+++ ColouredPoint.shader
@@ -0,0 +1,44 @@
+﻿Shader "Custom/ColouredPoint"
+{
+    Properties
+    {
+        _Glossiness ("Smoothness", Range(0,1)) = 0.5
+        _Metallic ("Metallic", Range(0,1)) = 0.0
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 200
+
+        CGPROGRAM
+        // Physically based Standard lighting model, and enable shadows on all light types
+        #pragma surface surf Standard fullforwardshadows
+
+        // Use shader model 3.0 target, to get nicer looking lighting
+        #pragma target 3.0
+
+        struct Input
+        {
+			float3 worldPos;
+        };
+
+        half _Glossiness;
+        half _Metallic;
+
+        UNITY_INSTANCING_BUFFER_START(Props)
+            // put more per-instance properties here
+        UNITY_INSTANCING_BUFFER_END(Props)
+
+        void surf (Input IN, inout SurfaceOutputStandard o)
+        {
+            // Albedo comes from a texture tinted by color
+            // Metallic and smoothness come from slider variables
+			o.Albedo.rb = IN.worldPos.xy * 0.5 + 0.5;
+            o.Metallic = _Metallic;
+            o.Smoothness = _Glossiness;
+            o.Alpha = 1;
+        }
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
---
+++ Graph.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour
+{
+    public Transform pointPrefab;
+    [Range(10, 100)] public int res = 10;
+    private Transform[] points;
+    public float speed = 10f;
+
+    void Awake()
+    {
+        float step = 2f / res;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.y = 0;
+        position.z = 0;
+        points = new Transform[res];
+       for(int i = 0; i < points.Length; i++)
+            {
+            Transform point = Instantiate(pointPrefab);
+
+            position.x = i * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+
+            points[i] = point;
+        }
+    }
+
+    void Update()
+    {
+        for(int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.position;
+            Vector3 target;
+            if (i == 0)
+            {
+                target = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+            }
+            else {
+                target = points[i-1].position;
+            }
+
+            float step = speed * Time.deltaTime * Vector3.Distance(position, target);
+            point.position = Vector3.MoveTowards(position, target, step);
+
+        }
+
+
+    }
+}
